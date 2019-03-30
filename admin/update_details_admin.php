35db9b9109a06c57a2e1bf35dfd806f8249f3058
Udit Namdev <unamdev0@gmail.com>
2019-03-31 01:50:45 +0530

    Functionality to update admin details for admin

---
+++ admin/update_details_admin.php
@@ -0,0 +1,20 @@
+<?php
+require('dbconn.php');
+$name=$_POST['name'];
+$email=$_POST['email'];
+$phone=$_POST['phone'];
+$city=$_POST['city'];
+$state=$_POST['state'];
+$address=$_POST['address'];
+$sql1="UPDATE LMS.user SET Name='$name', EmailId='$email', MobNo='$phone' WHERE RollNo='admin'";
+//echo $sql1;
+if($conn->query($sql1) === TRUE){
+    $_SESSION['message']='Details Updated';
+    $_SESSION['Name']=$name;
+}
+else {
+    $_SESSION['message']='Update failed';
+}
+$url="view_details_admin.php";
+header("location:".$url);
+?>
