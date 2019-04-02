3ad6a20194734c11224ac4f9ed8e153560029721
Udit Namdev <unamdev0@gmail.com>
2019-04-02 10:40:10 +0530

    Added detail's update of student for admin

---
+++ admin/update_details_member.php
@@ -0,0 +1,22 @@
+<?php
+require('dbconn.php');
+$n=$_POST['id'];
+$name=$_POST['name'];
+$status=$_POST['status'];
+if ($status=='Active')
+    $status='1';
+else
+    $status='0';
+$email=$_POST['emailid'];
+$mobno=$_POST['mobno'];
+$sql1="UPDATE LMS.user SET Name='$name', EmailId='$email', MobNo='$mobno',status='$status' WHERE RollNo='$n'";
+//echo $sql1;
+if($conn->query($sql1) === TRUE){
+    $_SESSION['message']='Details Updated';
+}
+else {
+    $_SESSION['message']='Update failed';
+}
+$url="view_details_member.php?Id=$n";
+header("location:".$url);
+?>
