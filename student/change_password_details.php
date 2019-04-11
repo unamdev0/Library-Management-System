b9647c50555f93f57cb51640c9540ac831dbf0e6
Udit Namdev <unamdev0@gmail.com>
2019-04-11 13:05:06 +0530

    Completed change password functionality for student

---
+++ student/change_password_details.php
@@ -0,0 +1,37 @@
+<?php
+require('dbconn.php');
+$no=$_SESSION['RollNo'];
+$c=$_POST['inputHorizontalSuccess'];
+$n=$_POST['inputHorizontalWarning'];
+$rn=$_POST['inputHorizontalReWarning'];
+//echo $rn;
+if(strcasecmp($n,$rn)==0 && !empty($n) && !empty($rn)) {
+    $sql = "select * from LMS.user where RollNo='$no'";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+    $x = $row['Password'];
+    //echo"$x";
+    //echo "$sql";
+    if (strcasecmp($x, $c) == 0 && strcasecmp($x,$n)!= 0 && !empty($x) && !empty($c)) {
+        $sql1 = "update LMS.user set Password ='$n' where RollNo='$no'";
+        //echo "$sql1";
+        if ($conn->query($sql1) === TRUE) {
+            $sql2 = "insert into LMS.notifications (RollNo,Msg) values('$no','Your Password has been updated')";
+            $conn->query($sql2);
+            $_SESSION['message']= 'Password Updated Successfully';
+        }
+        else {//echo $conn->error;
+            $_SESSION['message']='Error! Try again';
+        }
+    }
+    else{
+        $_SESSION['message']= 'Incorrect Current Password';
+    }
+}
+else{
+    $_SESSION['message']= 'New Password and Re-typed Password does not match';
+}
+//echo $_SESSION['message'];
+$url="change_password.php";
+header("location:".$url);
+?>
