f444b2912730b63d1523738f1483a7c96345a1b4
Udit Namdev <unamdev0@gmail.com>
2019-04-20 00:02:31 +0530

    Completed recommendation

---
+++ student/recommendation_details.php
@@ -0,0 +1,21 @@
+<?php
+    require('dbconn.php');
+    $rollno=$_SESSION['RollNo'];
+    $name=$_POST['inputHorizontalSuccess'];
+    $description=$_POST['inputHorizontalWarning'];
+    $sql1="insert into LMS.recommendations(RollNo,Book_Name,Description) values ('$rollno','$name','$description')";
+
+    if($conn->query($sql1) === TRUE){
+        $sql2 = "insert into LMS.notifications (RollNo,Msg) values('$rollno','Your recommendation for book $name has been sent')";
+        $conn->query($sql2);
+        $_SESSION['message']= 'Recommendation Sent';
+    }
+    else
+    {//echo $conn->error;
+        $_SESSION['message']= 'Error!';
+    }
+    echo $sql1;
+//echo $_SESSION['message'];
+$url="recommendation.php";
+header("location:".$url);
+?>
